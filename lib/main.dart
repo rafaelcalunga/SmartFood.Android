0b2db8e00e85d2e8137f2ab9ac8cdb08e0fea87f
Rafael Calunga <rafaelcalunga@gmail.com>
2022-07-31 13:18:02 -0400

    List layout improvement

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
+      debugShowCheckedModeBanner: false,
       home: const RecipesView(title: 'Recipes'),
     );
   }
--- lib/views/recipe_view.dart
+++ lib/views/recipe_view.dart
@@ -13,9 +13,45 @@ class RecipeView extends StatefulWidget {
 class _RecipeViewState extends State<RecipeView> {
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(widget.recipe.name),
-      subtitle: Text(widget.recipe.category.name),
+    return Card(
+      elevation: 1.0,
+      child: ListTile(
+        leading: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.network(widget.recipe.photo)),
+        title: Text(widget.recipe.name),
+        subtitle: Column(
+          //VStack
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                const Icon(Icons.access_time, size: 18),
+                Text("${widget.recipe.preparationTime} min"),
+              ],
+            ),
+            Row(
+              children: [
+                const Icon(Icons.restaurant, size: 18),
+                Text("${widget.recipe.servings} persons"),
+              ],
+            ),
+            DecoratedBox(
+              decoration: ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  color: Colors.pink),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Text(widget.recipe.category.name,
+                    style: const TextStyle(color: Colors.white)),
+              ),
+            ),
+          ],
+        ),
+        trailing: const Icon(Icons.chevron_right),
+      ),
     );
   }
 }
